a00de72b84c5cbb00835ef598c669a1630a536be
jupl <juan.placencia.512@gmail.com>
2015-01-17 17:37:23 -0600

    Enable tabbar/projectile combination

--- init/appearance/tabbar.el
+++ init/appearance/tabbar.el
@@ -12,7 +12,8 @@
 
 (defun tabbar/-init ()
   "Set up tabbar."
-  (global-set-key (kbd "C-<tab>") 'tabbar-mode))
+  (global-set-key (kbd "C-<tab>") 'tabbar-mode)
+  (with-eval-after-load 'projectile (tabbar/-init-projectile)))
 
 (defun tabbar/-init-evil ()
   "Set up tabbar with evil."
@@ -20,6 +21,10 @@
   (define-key evil-normal-state-map (kbd "C-k") 'tabbar-ruler-forward)
   (define-key evil-normal-state-map (kbd "C-l")
     'tabbar-ruler-tabbar-forward-group))
+
+(defun tabbar/-init-projectile ()
+  "Set up projectile integration with tabbar."
+  (tabbar-ruler-group-by-projectile-project))
 
 (with-eval-after-load 'evil (tabbar/-init-evil))
 (with-eval-after-load 'tabbar-ruler (tabbar/-init))
