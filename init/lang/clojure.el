1775e6bcf04357956d79890889bcc6c8219ef6b9
jupl <juan.placencia.512@gmail.com>
2015-01-18 02:16:26 -0600

    Add some keybindings for Evil

--- init/lang/clojure.el
+++ init/lang/clojure.el
@@ -17,7 +17,20 @@
   "Hide Clojure related packages in modeline."
   (with-eval-after-load 'cider (diminish 'cider-mode)))
 
+(defun clojure/-init ()
+  "Set up Clojure."
+  (with-eval-after-load 'evil-leader (clojure/-init-evil-leader)))
+
+(defun clojure/-init-evil-leader ()
+  "Set up evil leader keys for Clojure."
+  (with-eval-after-load 'cider
+    (evil-leader/set-key-for-mode 'clojure-mode
+      "cs" 'cider-jack-in
+      "cq" 'cider-quit
+      "cr" 'cider-load-buffer)))
+
 (add-hook 'lisp/hook 'clojure/-run-hook)
 (with-eval-after-load 'diminish (clojure/-diminish))
+(with-eval-after-load 'clojure-mode (clojure/-init))
 
 ;;; clojure.el ends here
